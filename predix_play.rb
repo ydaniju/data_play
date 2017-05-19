0d3fa0487c776839b2f52846e82b8c9aa7e4313a
andela-ydaniju <yusuf.daniju@andela.com>
2017-05-19 22:24:40 +0100

    clean up workspace

--- predix_play.rb
+++ predix_play.rb
@@ -56,49 +56,64 @@ def first_payload(tag)
   JSON.generate(hash)
 end
 
-tags = %w(
-  WT001_ACTIVE_POWER
-  WT001_AMBIENT_TEMP
-  WT001_HYDRAULIC_OIL_TEMP
-  WT001_PHASE_CURRENT_A
-  WT001_PHASE_CURRENT_B
-  WT001_PHASE_CURRENT_C
-  WT001_PHASE_VOLTAGE_A
-  WT001_PHASE_VOLTAGE_B
-  WT001_PHASE_VOLTAGE_C
-  WT001_REACTIVE_POWER
-  WT001_TURBINE_STATUS
-  WT001_WIND_DIRECTION
-  WT001_WIND_SPEED
+wind_turbines = %w(
+  WT001
+  WT002
+  WT003
+  WT004
+  WT005
+  WT006
+  WT007
+  WT008
+  WT009
+  WT010
 )
 
-hash = Hash.new([])
-tags.each do |tag|
-  payload_first = first_payload(tag)
-  first_point = doQuery(payload_first, ts_url, uaa_url, token, zone_id)
-  start_date = first_point[0][0]
-  payload = {
-    cache_time:  0,
-    tags: [{ name: tag, order: 'asc' }],
-    start: start_date
-  }
+10.times do |n|
+  tags = [
+    "#{wind_turbines[n]}_ACTIVE_POWER",
+    "#{wind_turbines[n]}_AMBIENT_TEMP",
+    "#{wind_turbines[n]}_HYDRAULIC_OIL_TEMP",
+    "#{wind_turbines[n]}_PHASE_CURRENT_A",
+    "#{wind_turbines[n]}_PHASE_CURRENT_B",
+    "#{wind_turbines[n]}_PHASE_CURRENT_C",
+    "#{wind_turbines[n]}_PHASE_VOLTAGE_A",
+    "#{wind_turbines[n]}_PHASE_VOLTAGE_B",
+    "#{wind_turbines[n]}_PHASE_VOLTAGE_C",
+    "#{wind_turbines[n]}_REACTIVE_POWER",
+    "#{wind_turbines[n]}_TURBINE_STATUS",
+    "#{wind_turbines[n]}_WIND_DIRECTION",
+    "#{wind_turbines[n]}_WIND_SPEED"
+  ]
 
-  series = doQuery(JSON.generate(payload), ts_url, uaa_url, token, zone_id)
-  series.each_with_index do |e, _i|
-    hash[e[0]] += [e[1]]
+  hash = Hash.new([])
+  tags.each do |tag|
+    payload_first = first_payload(tag)
+    first_point = doQuery(payload_first, ts_url, uaa_url, token, zone_id)
+    start_date = first_point[0][0]
+    payload = {
+      cache_time:  0,
+      tags: [{ name: tag, order: 'asc' }],
+      start: start_date
+    }
+
+    series = doQuery(JSON.generate(payload), ts_url, uaa_url, token, zone_id)
+    series.each_with_index do |e, _i|
+      hash[e[0]] += [e[1]]
+    end
   end
-end
 
-headers = tags.unshift('timestamp')
-arr = []
-a = 0
-hash.each do |k, v|
-  arr[a] = [k, v].flatten
-  a += 1
-end
+  headers = tags.unshift('timestamp')
+  arr = []
+  a = 0
+  hash.each do |k, v|
+    arr[a] = [k, v].flatten
+    a += 1
+  end
 
-CSV.open('data.csv', 'wb', write_headers: true, headers: headers) do |csv|
-  arr.each do |elem|
-    csv << elem
+  CSV.open("#{wind_turbines[n]}.csv", 'wb', write_headers: true, headers: headers) do |csv|
+    arr.each do |elem|
+      csv << elem
+    end
   end
 end
