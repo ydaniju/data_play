d64ac6930d1d8c96d60bf2ecad45c6bfdd2db842
andela-ydaniju <yusuf.daniju@andela.com>
2017-05-20 07:05:51 +0100

    ignore html files

--- play.py
+++ play.py
@@ -2,10 +2,10 @@ import pandas as pd
 wt1 = pd.read_csv("WT002.csv")
 from bokeh.charts import Scatter, output_file, show
 
-data = dict(active_power = wt1['WT002_ACTIVE_POWER'], wind_direction=wt1['WT002_WIND_DIRECTION'])
+data = dict(active_power = wt1['WT002_ACTIVE_POWER'], wind_speed=wt1['WT002_WIND_SPEED'])
 p = Scatter(
-  data, x='active_power', y='wind_direction', xlabel='Active Power',
-  ylabel='Wind Direction', title='Active Power vs Wind Direction', color='green'
+  data, y='active_power', x='wind_speed', ylabel='Active Power',
+  xlabel='Wind Speed', title='Active Power vs Wind Speed', color='green'
 )
 
 output_file("002_active_power.html")
