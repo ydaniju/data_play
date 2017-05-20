446235dba739fa259bf6e52d1a0367b20b6477b0
andela-ydaniju <yusuf.daniju@andela.com>
2017-05-20 08:22:20 +0100

    introduce jupyter notebook

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import pandas as pd
 wt1 = pd.read_csv("WT002.csv")
 from bokeh.charts import Scatter, output_file, show
 
-data = dict(active_power = wt1['WT002_ACTIVE_POWER'], wind_speed=wt1['WT002_WIND_SPEED'])
+data = dict(active_power = wt1['WT002_ACTIVE_POWER'], wind_speed=wt1['WT002_TURBINE_STATUS'])
 p = Scatter(
   data, y='active_power', x='wind_speed', ylabel='Active Power',
   xlabel='Wind Speed', title='Active Power vs Wind Speed', color='green'
